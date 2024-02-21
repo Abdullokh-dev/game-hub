f0321283c1f050814d304cb32b6697117e4a1988
Frontend Developer <abdulloh3525Dev@gmail.com>
2024-02-21 17:33:48 +0300

    Build a Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,23 @@ function App() {
 
   return (
     <>
-      <button className="btn btn-warning">button</button>
+      <div className="container-fluid bg-warning">
+        <header>
+          Nav
+        </header>
+      </div>
+
+      <main className="container-fluid">
+        <div className="row">
+          <div className="d-none d-xl-block col-12 col-xl-6 bg-success">
+            <aside>Aside</aside>
+          </div>
+
+          <div className="col-12 col-xl-6 bg-secondary">
+            Main
+          </div>
+        </div>
+      </main>
     </>
   )
 }
