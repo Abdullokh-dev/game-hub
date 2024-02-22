79808d5c648797b2429a60af68de963c7a4f1318
Frontend Developer <abdulloh3525Dev@gmail.com>
2024-02-22 14:00:27 +0300

    Create a generic data fetching hook

--- src/components/Games/Games.tsx
+++ src/components/Games/Games.tsx
@@ -3,14 +3,14 @@ import GameCard from "../GameCard/GameCard";
 import GameCardSkeleton from "../GameCard/GameCardSkeleton";
 
 function Games() {
-  const { error, games, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xxl-4">
         {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
       </div>
