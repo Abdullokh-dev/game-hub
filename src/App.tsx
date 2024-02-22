4298fcbaeffe11778d2370492435bf847636721c
Frontend Developer <abdulloh3525Dev@gmail.com>
2024-02-22 13:45:01 +0300

    Fetch Genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Games from "./components/Games/Games";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -9,11 +10,13 @@ function App() {
 
         <main>
           <div className="row">
-            <div className="d-none d-xl-block col-12 col-xl-6">
-              <aside>Aside</aside>
+            <div className="d-none d-xl-block col-12 col-xl-3 col-xxl-2">
+              <aside>
+                <GenreList />
+              </aside>
             </div>
 
-            <div className="col-12 col-xl-6">
+            <div className="col-12 col-xl-9 col-xxl-10">
               <Games />
             </div>
           </div>
